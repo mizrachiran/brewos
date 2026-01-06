81151866cb6e438317e9942d7076b75e289e176a
Ran Mizrachi <mizrachiran@gmail.com>
2026-01-06 13:25:18 +0200

    chore: bump version to 0.8.35

--- src/pico/include/config.h
+++ src/pico/include/config.h
@@ -17,7 +17,7 @@
 // -----------------------------------------------------------------------------
 #define FIRMWARE_VERSION_MAJOR      0
 #define FIRMWARE_VERSION_MINOR      8
-#define FIRMWARE_VERSION_PATCH      34
+#define FIRMWARE_VERSION_PATCH      35
 #define PICO_VERSION_MAJOR      FIRMWARE_VERSION_MAJOR
 #define PICO_VERSION_MINOR      FIRMWARE_VERSION_MINOR
 #define PICO_VERSION_PATCH      FIRMWARE_VERSION_PATCH
