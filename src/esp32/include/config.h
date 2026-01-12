7032d26609350553ced00a93b1e21b934296d160
Ran Mizrachi <mizrachiran@gmail.com>
2026-01-12 11:13:49 +0200

    chore: bump version to 0.8.47

--- src/esp32/include/config.h
+++ src/esp32/include/config.h
@@ -14,9 +14,9 @@
 // -----------------------------------------------------------------------------
 #define ESP32_VERSION_MAJOR     0
 #define ESP32_VERSION_MINOR     8
-#define ESP32_VERSION_PATCH     46
+#define ESP32_VERSION_PATCH     47
 #define ESP32_VERSION_PRERELEASE "beta.1"  // Empty string "" for stable releases
-#define ESP32_VERSION           "0.8.46"
+#define ESP32_VERSION           "0.8.47"
 
 // Build timestamp (set at compile time)
 // Format: "Dec 12 2024" and "14:30:45"
